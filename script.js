753153abdfe2009d46dbed16ab3647c82e8fd0e8
rishabh173 <41022485+rishabh173@users.noreply.github.com>
2019-03-03 19:00:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ $(function(){
         
         $('#coverquote').text('A random generic quote on whatever the client wants!');
         $('#coverdescription').hide();
-        $(this).css('background-image','url(sample/img5.png)');
+        $(this).css('background-image','url(img5.png)');
         
     }, function() {
         $('#coverquote').text('Company Name');
         $('#coverdescription').show();
-        $(this).css('background-image','url(sample/ban1.png)');
+        $(this).css('background-image','url(ban1.png)');
     });
 
 
